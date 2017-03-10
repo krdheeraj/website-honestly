fdb67fc9d753792916bd45f2267c99b074ceebe4
Graham Mendick <graham.mendick@gmail.com>
2017-03-10 14:11:06 +0000

    Commented the service worker code

--- site/sw.js
+++ site/sw.js
@@ -10,6 +10,7 @@ const OFFLINE_URL = urlPrefix + '/offline/';
 
 const stateNavigator = new StateNavigator(routeDefinitions);
 
+// Caches the Home and Offline pages on installation of service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -18,6 +19,7 @@ self.addEventListener('install', event => {
   );
 });
 
+// Deletes old caches on activation of new service worker
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keyList => {
@@ -39,6 +41,7 @@ self.addEventListener('fetch', event => {
         if (!res || res.status !== 200 || res.type !== 'basic') {
           return res;
         }
+        // Caches assets on demand
         if (/\.(css|js|png|jpg|woff2)$/i.test(url)) {
           const responseToCache = res.clone();
 
@@ -50,14 +53,23 @@ self.addEventListener('fetch', event => {
         return res;
       }).catch(() => {
         const link = url.substring(urlPrefix.length);
+        // Checks if the URL doesn't contains '.'.
+        // If it doesn't it's a page request, e.g., https://domain/about-us
+        // If it does it's an asset request, e.g., https://domain/logo.png
         if (!/\./.test(link)) {
           try {
+            // Checks if the URL is part of website-honestly navigation
+            // If it is, e.g., https://domain/about-us, then it returns the
+            // Home page and lets website-honestly navigate to the component
+            // If it isn't, e.g., https://domain/blog, then it returns the
+            // Offline page
             stateNavigator.parseLink(link);
             return caches.match(HOMEPAGE_URL);
           } catch (e) {
             return caches.match(OFFLINE_URL);
           }
         } else {
+          // Returns the asset from the cache
           return caches.match(event.request);
         }
       })
